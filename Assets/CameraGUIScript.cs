86483baff5c8b988ffeee77de19614b36d1c613d
Chris Hamer <chamer1@umbc.edu>
2014-09-21 14:56:03 -0400

    Inventory stuff.

---
+++ Assets/CameraGUIScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraGUIScript : MonoBehaviour {
+
+	public GameObject inv;
+
+	// Use this for initialization
+	void Start () {
+		guiText.enabled = true;
+		guiText.text = "GFDNSKGHJFEKDSJ";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		int f = 1;
+		foreach (GameObject i in inv.GetComponent<InventoryScript>().items) {
+			GUI.Label (new Rect (10, 15*f++, 1000, 100),i.GetComponent<ItemData>().getDescription());
+		}
+	}
+}
--- Assets/Scripts/InventoryScript.cs
+++ Assets/Scripts/InventoryScript.cs
@@ -6,12 +6,16 @@ public class InventoryScript : MonoBehaviour {
 
 	public GameObject item;
 
-	public List<GameObject> x = new List<GameObject>();
+	public List<GameObject> items = new List<GameObject>();
 
 	// Use this for initialization
 	void Start () {
-		x.Add ((GameObject) Instantiate (item,Vector3.zero,Quaternion.identity));
-		x[0].GetComponent<ItemData> ().setDescription("AHAHAHAHAH");
+		items.Add ((GameObject) Instantiate (item,Vector3.zero,Quaternion.identity));
+		items[0].GetComponent<ItemData> ().setDescription("AHAHAHAHAH");
+		items.Add ((GameObject) Instantiate (item,Vector3.zero,Quaternion.identity));
+		items[1].GetComponent<ItemData> ().setDescription("THE SECOND ITEM");
+		items.Add ((GameObject) Instantiate (item,Vector3.zero,Quaternion.identity));
+		items[2].GetComponent<ItemData> ().setDescription("OH GOD WE HAVE THREE ITEMS");
 //		Debug.Log (x[0].GetComponent<ItemData> ().getDescription());
 	}
 	
@@ -21,7 +25,7 @@ public class InventoryScript : MonoBehaviour {
 
 	/* Adds an item to the inventory */
 	public void add(GameObject newitem) {
-		x.Add (newitem);
+		items.Add (newitem);
 	}
 
 	/* Removes an item from the inventory. *
@@ -29,7 +33,7 @@ public class InventoryScript : MonoBehaviour {
 	 * sucessful, returns null if the item *
 	 * was not found.                      */
 	public GameObject remove(GameObject removeditem) {
-		if (x.Remove (removeditem))
+		if (items.Remove (removeditem))
 			return removeditem;
 		return null;
 	}
@@ -37,7 +41,7 @@ public class InventoryScript : MonoBehaviour {
 	/* Checks if the player has the specified *
 	 * item, and returns true if they do.     */
 	public bool hasItem(GameObject searchitem) {
-		if(x.Contains(searchitem))
+		if(items.Contains(searchitem))
 			return true;
 		return false;
 	}
